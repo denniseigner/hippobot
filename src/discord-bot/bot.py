efe01769831ab6b70904442e806f1991952c52fe
Dennis Eigner <dennis@deigner.eu>
2022-04-30 02:22:50 +0000

    Generate verification code

--- src/discord-bot/bot.py
+++ src/discord-bot/bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 
 from dotenv import load_dotenv
 
@@ -13,8 +14,22 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print("Message from {0.author}: {0.content}".format(message))
 
-        if message.content == "$link":
-            pass
+        if message.content == "$link-account":
+            await message.reply("I have sent you a dm to link up your account!")
+            await message.author.send(
+                "To link up your account send the following message to the user 'Beardedhippo' in the osu! ingame chat:"
+            )
+            await message.author.send(f"$verify {self.generate_verification_code()}")
+
+    def generate_verification_code(self):
+        verification_code_lenght = 32
+        verification_code = ""
+
+        for _ in range(verification_code_lenght):
+            random_integer = random.randint(97, 122)
+            verification_code += chr(random_integer)
+
+        return verification_code
 
 
 client = MyClient()
